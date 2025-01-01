9c3a89678b7c314ad9ef454d03969c52e5d03332
lianqingqian <lian2088486180@163.com>
2025-01-01 23:25:14 +0800

    feat:完成编辑添加删除部门modal渲染

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
-import { type TreeProps, Tree } from 'ant-design-vue';
-import { ref, useId } from 'vue';
+import { DownOutlined } from '@ant-design/icons-vue';
+import { type MenuProps, type TreeProps, Dropdown, Flex, Menu, Modal, Tree, TypographyText } from 'ant-design-vue';
+import type { MenuItemType } from 'ant-design-vue/es/menu/src/interface';
+import { h, ref, useId } from 'vue';
+
+import DepartmentModal from './components/modal.vue';
 
 defineOptions({
   name: "DepartmentPage"
@@ -10,30 +14,37 @@ const departmentTree = ref<TreeProps['treeData']>([
   {
     key: useId(),
     title: "字节跳动",
+    managerName: "lianqq",
     children: [
       {
         key: useId(),
         title: "总裁办",
+        managerName: "lianqq",
       },
       {
         key: useId(),
-        title: '行政部'
+        title: '行政部',
+        managerName: "lianqq",
       },
       {
         key: useId(),
         title: '人事部',
+        managerName: "lianqq",
         children: [
           {
             key: useId(),
-            title: "财务核算部"
+            title: "财务核算部",
+            managerName: "lianqq",
           },
           {
             key: useId(),
-            title: "税务管理部"
+            title: "税务管理部",
+            managerName: "lianqq",
           },
           {
             key: useId(),
-            title: "薪资管理部"
+            title: "薪资管理部",
+            managerName: "lianqq",
           }
         ]
       }
@@ -41,21 +52,115 @@ const departmentTree = ref<TreeProps['treeData']>([
   }
 ])
 
+const operations: MenuProps['items'] = [
+  {
+    key: "add",
+    label: "添加子部门",
+  },
+  {
+    key: "edit",
+    label: "编辑部门",
+  },
+  {
+    key: "delete",
+    label: "删除",
+  }
+]
+
+
+const modalOpen = ref(false)
+const modalType = ref<'add' | 'edit'>("add")
+
+
+const handleOpenModal = (type: "add" | "edit") => {
+  modalOpen.value = true
+  modalType.value = type
+}
+
+const handleAddSubDepartment = (key: string | number) => {
+  console.log("添加子部门:", key)
+  handleOpenModal("add")
+}
+
+const handleEditDepartment = (key: string | number) => {
+  console.log("编辑部门", key)
+  handleOpenModal("edit")
+}
+
+const handleDeleteDepartment = (key: string | number) => {
+  console.log("删除部门", key)
+
+  Modal.error({
+    title: "删除部门",
+    content: "确定要删除该部门吗？",
+    okText: "确定删除",
+    okButtonProps: {
+      danger: true
+    },
+    cancelText: "取消",
+    onOk: () => {
+      console.log("删除部门", key)
+    }
+  })
+}
+
+const operationClickMap = {
+  add: handleAddSubDepartment,
+  edit: handleEditDepartment,
+  delete: handleDeleteDepartment
+}
+
+const handleOperationClick = (info: MenuItemType, key: string | number) => {
+  console.log("点击菜单", info)
+  // 触发对应的事件
+  operationClickMap[info.key as keyof typeof operationClickMap](key)
+}
+
+
+
 
 </script>
 
 
 <template>
   <div class="department-wrapper">
-    <Tree class="draggable-tree" default-expand-all draggable block-node :tree-data="departmentTree" />
-
+    <Tree class="department-tree" default-expand-all draggable block-node :tree-data="departmentTree">
+      <template #title="{ title, managerName, key }">
+        <Flex class="department-tree-item" justify="space-between">
+          <TypographyText>{{ title }}</TypographyText>
+          <Flex gap="middle">
+            <TypographyText type="secondary">{{ managerName }}</TypographyText>
+            <Dropdown :overlay="h(Menu, { items: operations, onClick: (info) => handleOperationClick(info, key) })"
+              :arrow="{ pointAtCenter: true }">
+              <TypographyText type="secondary">
+                操作
+                <DownOutlined />
+              </TypographyText>
+            </Dropdown>
+          </Flex>
+        </Flex>
+      </template>
+    </Tree>
+    <DepartmentModal v-model:open="modalOpen" v-model:type="modalType" :tree-data="departmentTree" />
   </div>
 </template>
 
 <style lang="less" scoped>
-.department-wrapper {
-  height: 100%;
-  padding: var(--spacing-large);
-  background-color: var(--color-background);
+.department {
+  &-wrapper {
+    height: 100%;
+    padding-block: calc(var(--spacing-large) * 2);
+    padding-inline: calc(var(--spacing-large) + 10%);
+    background-color: var(--color-background);
+  }
+
+  &-tree {
+    max-width: 60%;
+  }
+
+}
+
+:deep(.ant-tree-treenode) {
+  padding-block: var(--spacing-small);
 }
 </style>
